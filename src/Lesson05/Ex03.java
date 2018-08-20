5047ad732b97234e46fefdf573b7fc21e564c471
oleksalab <oleksalab@gmail.com>
2018-08-20 17:54:39 +0300

    добавил допзадание урок 5

--- src/Lesson05/Ex03.java
+++ src/Lesson05/Ex03.java
@@ -1,29 +1,92 @@
 package Lesson05;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
+/*
+рекурсивно просмотреть содержимое папки и её подпапок, создать индексы по типам файлов
+ */
 public class Ex03 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        String path = "src";
-        path = "C:/Program Files/";
+        // папка с файлами для анализа
+        String dirPath = "D:/Temp";
+        // папка для индекса (список файлов с путями)
+        String dirIndex = dirPath + "/.index";
 
-        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
 
-        listAll(path, result);
+        try {
+            // формируем список всех файлов
+            listAll(dirPath, list);
 
-        for (String line : result) {
+        } catch (IOException ioe) {
 
-            System.out.println(line);
+            ioe.printStackTrace();
+        }
+
+        // создаём папку для индекса
+        Path path = Paths.get(dirIndex);
+        if (!Files.exists(path)) {
+
+            try {
+
+                Files.createDirectory(path);
+
+            } catch (IOException ioe) {
+
+                ioe.printStackTrace();
+            }
+        }
+
+        // строим индекс по файлам
+        for (String line : list) {
+
+            String[] parts = line.split(" > ");
+            if (parts[0].equals("File")) {
+
+                String filePath = parts[1];
+                String fileType = filePath.substring(filePath.lastIndexOf('.') + 1);
+                String indexFile = dirIndex + "/" + fileType;
+
+                System.out.println(fileType + " > " + filePath);
+
+                try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(indexFile, true)))) {
+
+                    out.println(filePath);
+
+                } catch (FileNotFoundException fnfe) {
+
+                    //fnfe.printStackTrace();
+
+                } catch (IOException ioe) {
+
+                    //ioe.printStackTrace();
+
+                }
+            }
         }
     }
 
-    /*
-    TODO: разложить согласно форматов пути к файлам по файлам с названием этого формата
-     */
+
+    // запись текстового файла
+    private static void saveFile(String filePath, String text) {
+
+        try (BufferedWriter file = new BufferedWriter(new FileWriter(filePath))) {
+
+            file.write(text);
+            file.newLine();
+
+        } catch (IOException ioe) {
+
+            ioe.printStackTrace();
+        }
+    }
+
     private static void listAll(String path, ArrayList<String> result) throws IOException {
 
         File dir = new File(path);
@@ -36,11 +99,11 @@ public class Ex03 {
 
             if (item.isFile()) {
 
-                result.add("File : " + item.getCanonicalPath());
+                result.add("File > " + item.getCanonicalPath());
 
             } else {
 
-                result.add("Dir : " + item.getCanonicalPath());
+                result.add("Dir > " + item.getCanonicalPath());
 
                 listAll(item.getCanonicalPath(), result);
             }
